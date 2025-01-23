8165625e873c13f95450793c8505d77dee03651f
aka3nd <fileaing1@gmail.com>
2025-01-23 09:10:05 +0700

    add arr9

---
+++ basic_cpp/array/arr9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main (int argc, char *argv[]) {
+  //belajar array
+  int angka[5] = {1,2,3,4,5};
+  std::cout << angka[0] << std::endl;
+  std::cout << angka[1] << std::endl;
+  std::cout << angka[2] << std::endl;
+  std::cout << angka[3] << std::endl;
+  std::cout << angka[4] << std::endl;
+  return 0;
+}
